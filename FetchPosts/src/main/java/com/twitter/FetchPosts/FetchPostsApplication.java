7295b3125c2d6bbac12fbe2577ae1af4694e4f9d
ban458106 <ban458106@10.10.10.46>
2026-02-24 11:17:04 +0530

    fetch feed and post services added

---
+++ FetchPosts/src/main/java/com/twitter/FetchPosts/FetchPostsApplication.java
@@ -0,0 +1,13 @@
+package com.twitter.FetchPosts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchPostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchPostsApplication.class, args);
+	}
+
+}
---
+++ FetchPosts/src/test/java/com/twitter/FetchPosts/FetchPostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitter.FetchPosts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchPostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
